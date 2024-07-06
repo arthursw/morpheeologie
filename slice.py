c63377a1468c7b2415561c6943db4b1170156569
Arthur Masson <arthur.masson@inria.fr>
2024-07-06 19:33:59 +0200

    improve trajectories so that min dist between slices ; go up only when traveling long distances

--- slice.py
+++ slice.py
@@ -15,6 +15,9 @@ speed = 3000
 inject_speed = 3000
 plane_pause = 500 #in ms
 
+# Threshold above which the head will go up, travel, and go down
+small_move_length = 5 # in mm
+
 mesh = trimesh.load('models/female_hand.glb', force='mesh')
 
 def put_mesh_in_bounds(mesh, box_size):
@@ -56,13 +59,19 @@ def put_mesh_in_bounds(mesh, box_size):
 # def create_trajetory(slice, step):
 #     return slice.vertices
 
-def create_trajetory(slice, step):
+def find_closest_vertex(point, vertices):
+    # Find the closest vertex (cumpte the distances from the point to the others)
+    distances = [np.linalg.norm(point-vi) for vi in vertices]
+    return np.argmin(distances)
+
+def create_trajetory(slice, trajectories):
     vertices = list(slice.vertices)
-    v = vertices.pop()
+    # The first point to consider is the closest one from the last point of the last trajectory ; or the last point if there are no trajectory
+    v = vertices.pop() if len(trajectories) == 0 else vertices[find_closest_vertex(trajectories[-1][-1], vertices)]
+    # Put the point in trajectory, then add the closest one, then continue until there are no points
     trajectory = [v]
     while len(trajectory) < len(slice.vertices):
-        distances = [np.linalg.norm(v-vi) for vi in vertices]
-        v = vertices.pop(np.argmin(distances))
+        v = vertices.pop(find_closest_vertex(v, vertices))
         trajectory.append(v)
     return trajectory
 
@@ -73,7 +82,7 @@ def slice_mesh(mesh, step_z, step, box_size):
         slice = mesh.section(plane_origin=[0, -box_size[1]/2 + current_distance, 0], plane_normal=[0,1,0])
         current_distance += step_z
         if slice:
-            trajectories.append(create_trajetory(slice, step))
+            trajectories.append(create_trajetory(slice, trajectories))
     return trajectories
 
 def quad_to_tri(quad):
@@ -114,9 +123,10 @@ def trajectories_to_gcode(trajectories):
     zs = []
     height = 0
     
+    current_position = None
     for trajectory in trajectories:
         
-        for p in trajectory:
+        for i, p in enumerate(trajectory):
             # go to position
             x = p[0] + home_pos[0]
             # Warning X in the 3D space becomes X in 2D, but Z in the 3D space becomes Y
@@ -126,14 +136,27 @@ def trajectories_to_gcode(trajectories):
             zs.append(pos_z_down+height)
             zs.append(pos_z_up+height)
 
+            # Fast way: go to XY position, inject, go to next position. Only when next position is far: go up, move, and go down
+            
+            target_position = np.array([x, y])
+            target_is_far = current_position is None or np.linalg.norm(target_position-current_position) > small_move_length
+            
+            # up if too far
+            if target_is_far:
+                commands.append(f'G0 Z{pos_z_up+height} F{speed:.2f}')
+
+            # move
             commands.append(f'G0 X{x:.2f} Y{y:.2f} F{speed:.2f}')
-            # down
-            commands.append(f'G0 Z{pos_z_down+height}')
+
+            # down if too far
+            if target_is_far:
+                commands.append(f'G0 Z{pos_z_down+height}')
+            
             # inject
             commands.append(f'G0 E{inject_amount} F{inject_speed:.2f}')
             commands.append(f'G0 E{vacuum_amount} F{inject_speed:.2f}')
-            # up
-            commands.append(f'G0 Z{pos_z_up+height} F{speed:.2f}')
+            
+            current_position = target_position
 
         # pause
         commands.append(f'G4 P{plane_pause}')
